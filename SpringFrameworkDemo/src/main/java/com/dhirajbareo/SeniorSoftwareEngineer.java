99db23cdcf636ab69c30e2d379ec1718b5959d7b
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-11 23:52:05 +0530

    implement Bean life by  BeanNameAware

--- SpringFrameworkDemo/src/main/java/com/dhirajbareo/SeniorSoftwareEngineer.java
+++ SpringFrameworkDemo/src/main/java/com/dhirajbareo/SeniorSoftwareEngineer.java
@@ -1,10 +1,13 @@
 package com.dhirajbareo;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+
 @Component
-@Scope(scopeName="prototype")
-public class SeniorSoftwareEngineer implements Employee {
+@Scope(scopeName="prototype")   // Bean scope  
+public class SeniorSoftwareEngineer implements Employee,BeanNameAware{
 
 	
 	private String band; 
@@ -22,7 +25,15 @@ public class SeniorSoftwareEngineer implements Employee {
 	public void setBand(String band) {
 		this.band = band;
 	}
+	
+	
+	
 	public void assist() {
 		System.out.println("Senior Software Engineer is assisting");
+	}
+
+	public void setBeanName(String name) {
+		System.out.println("Set Bean name method is called ");
+		
 	}
 }
